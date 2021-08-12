f0e44ed2f9272746a80594b6289242fe1d04cd81
ahmadhah <54240600+ahmadhah@users.noreply.github.com>
2021-08-12 15:15:47 +0500

    .

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
 import './App.css';
 import React, { useState, useEffect } from 'react'
+import List from './List';
 
 function App() {
   const [text, settext] = useState('');
-  const [tasks, settasks] = useState(['ahmad', 'hannan', 'masood']);
+  const [list, setList] = useState([]);
+  const [editing, setediting] = useState(false);
+  const [idd, setidd] = useState('');
 
   function handleSubmit(e) {
-    e.preventDefault();
-    console.log(text);
+    {
+      console.log('handle submit func');
+      e.preventDefault();
+      const newItem = { id: new Date().getTime().toString(), title: text };
+      setList([...list, newItem]);
+      settext('');
+    }
+  }
+  function editi() {
+    console.log('editi')
+    setList(
+      list.map((item) => {
+        if (item.id === idd) {
+          return { ...item, title: text };
+        }
+        return item;
+      })
+    );
+    setediting(false);
+  }
+  function edit(id) {
     
+    setediting(true)
+    const spe = list.find((item) => item.id === id);
+    settext(spe.title)
+    setidd(spe.id)
 
+
+  }
+  function delet(id) {
+    const newList = list.filter((item) => item.id != id);
+    setList(newList)
   }
 
   return (
@@ -18,8 +49,15 @@ function App() {
       <div className="container">
         <form onSubmit={handleSubmit}>
           <input value={text} onChange={(e) => settext(e.target.value)} type="text" className="input" />
-          <button type="submit" className="btn btn-secondary">Add</button>
+          {editing ? <button onClick={editi} className="btn btn-secondary">Update</button> : <button className="btn btn-secondary">Add</button>}
+
         </form>
+        <ul className="list-group">
+        </ul>
+        {
+          list.map((item, i) => < div key={i}><li className="  list-group-item list-group-item-info">{item.title} <i className="inl medium material-icons" onClick={() => delet(item.id)}>close</i><i className="inl medium material-icons" onClick={() => edit(item.id)}> border_color</i>  </li></div>)
+        }
+        <List />
       </div>
     </div>
   );
