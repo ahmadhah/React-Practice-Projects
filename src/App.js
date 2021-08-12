9ab0332e9dd833cd0dd7998dbb63df9057ff27e7
ahmadhah <54240600+ahmadhah@users.noreply.github.com>
2021-08-12 10:58:55 +0500

    bw

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react'
 
 function App() {
+  const [text, settext] = useState('');
+  const [tasks, settasks] = useState(['ahmad', 'hannan', 'masood']);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(text);
+    
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">ToDo App</h1>
+      <div className="container">
+        <form onSubmit={handleSubmit}>
+          <input value={text} onChange={(e) => settext(e.target.value)} type="text" className="input" />
+          <button type="submit" className="btn btn-secondary">Add</button>
+        </form>
+      </div>
     </div>
   );
 }
