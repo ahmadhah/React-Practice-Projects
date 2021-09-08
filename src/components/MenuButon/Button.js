02fd623be140d7a128a67d1d7a2c6cf263fc4517
ahmadhah <54240600+ahmadhah@users.noreply.github.com>
2021-09-08 06:18:17 +0500

    .

--- src/components/MenuButon/Button.js
+++ src/components/MenuButon/Button.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './style.css'
-export default function Button({ menu, SetCategory }) {
 
+export default function Button({ menu, SetCategory }) {
     return (
         <div>
             <button type="button" className="btn btn-success" onClick={() => SetCategory(menu)}>{menu}</button>
