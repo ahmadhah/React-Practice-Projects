697acd3d38233baa92dbbb4100956c576e768a5e
ahmadhah <54240600+ahmadhah@users.noreply.github.com>
2021-10-13 07:19:34 +0500

    .

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ import { Provider } from "react-redux";
 
 //The created store
 const store = createStore(
-  allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 ReactDOM.render(
