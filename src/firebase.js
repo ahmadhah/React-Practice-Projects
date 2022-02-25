01ac93a6eff64e736b6eafcd5528435f9116023d
Ahmad Hannan <54240600+ahmadhah@users.noreply.github.com>
2022-02-25 14:35:05 +0500

    firebase.

--- src/firebase.js
+++ src/firebase.js
@@ -1,48 +1,41 @@
-
-
-// Import the functions you need from the SDKs you need
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "@firebase/firestore";
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+const firebaseConfig = {
+    apiKey: "AIzaSyAwKYPz4VFjzy6iO9KGjLTjTlejvb3O5x0",
+    authDomain: "disney-clone-d94ba.firebaseapp.com",
+    databaseURL: "https://disney-clone-d94ba-default-rtdb.firebaseio.com",
+    projectId: "disney-clone-d94ba",
+    storageBucket: "disney-clone-d94ba.appspot.com",
+    messagingSenderId: "511873480314",
+    appId: "1:511873480314:web:c596ad87d87f7e0ec46dc3",
+    measurementId: "G-MXY1LZP1CB"
+};
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
 
-// Get a list of cities from your database
-// async function getCities(db) {
-//     const citiesCol = collection(db, 'movies');
-//     const citySnapshot = await getDocs(citiesCol);
-//     const cityList = citySnapshot.docs.map(doc => doc.data());
-//     console.log(cityList)
-// }
+const provider = new GoogleAuthProvider();
+const storage = getStorage(app);
 
-// const cityList = getCities(db);
+export const signInWithGoogle = () => {
+    signInWithPopup(auth, provider)
+        .then((result) => {
+            const name = result.user.displayName;
+            const email = result.user.email;
+            const profilePic = result.user.photoURL;
 
-// export { cityList };
-// export default getCities;
+            localStorage.setItem("name", name);
+            localStorage.setItem("email", email);
+            localStorage.setItem("profilePic", profilePic);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+};
 
+export { db, auth, provider, storage };
 
-// import firebase from "firebase";
-
-// const firebaseConfig = {
-//     apiKey: "AIzaSyAwKYPz4VFjzy6iO9KGjLTjTlejvb3O5x0",
-//     authDomain: "disney-clone-d94ba.firebaseapp.com",
-//     databaseURL: "https://disney-clone-d94ba-default-rtdb.firebaseio.com",
-//     projectId: "disney-clone-d94ba",
-//     storageBucket: "disney-clone-d94ba.appspot.com",
-//     messagingSenderId: "511873480314",
-//     appId: "1:511873480314:web:c596ad87d87f7e0ec46dc3",
-//     measurementId: "G-MXY1LZP1CB"
-// };
-
-// const firebaseApp = firebase.initializeApp(firebaseConfig);
-// const db = firebaseApp.firestore();
-// const auth = firebase.auth();
-// const provider = new firebase.auth.GoogleAuthProvider();
-// const storage = firebase.storage();
-
-// export { auth, provider, storage };
-// export default db;
 
